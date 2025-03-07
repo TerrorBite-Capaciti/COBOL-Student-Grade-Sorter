596aa3fea0e045229509196940c33ddbf7552da6
Nizaam Haffejee <nizaam.h@icloud.com>
2025-03-07 14:27:50 +0200

    added files for student data records

--- Student-Grade-Sorter.cob
+++ Student-Grade-Sorter.cob
@@ -1,9 +1,9 @@
-      ******************************************************************
-      * Author: TerrorBite CAPACITI
-      * Date: 04 March 2025
-      * Purpose: Sort student grades into ascending order
-      * Compiler: cobc
-      ******************************************************************
+      *> ******************************************************************
+      *> * Author: TerrorBite CAPACITI
+      *> * Date: 04 March 2025
+      *> * Purpose: Sort student grades into ascending order
+      *> * Compiler: cobc
+      *> ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. STUDENT-GRADE-SORTER.
 
@@ -12,26 +12,30 @@
        FILE-CONTROL.
            SELECT PERSON-FILE ASSIGN TO 'students.csv'.
 
-      *****Below file is for processing a file with columns etc.
-           SELECT STUDENTS-FILE ASSIGN TO 'students_records.txt'.
+      *> *****Below file is for processing a file with columns etc.
+           SELECT INPUT-FILE ASSIGN TO 'students_file.csv'
+      *>      SELECT INPUT-FILE ASSIGN TO 'students_records.txt'
+                ORGANIZATION IS SEQUENTIAL.
 
-      *****Temp file for compiler to perform SORT operation*************
+      *> *****Temp file for compiler to perform SORT operation*************
            SELECT WORK-FILE ASSIGN TO 'temp_sort_file.tmp'.
 
-      *****Output file for when records have been sorted****************
-           SELECT SORTED-FILE ASSIGN TO 'sorted_student_records.txt'.
+      *> *****Output file for when records have been sorted****************
+           SELECT SORTED-OUTPUT-FILE ASSIGN TO
+                'sorted_student_records.txt'
+                ORGANIZATION IS SEQUENTIAL.
 
        DATA DIVISION.
        FILE SECTION.
-       FD STUDENTS-FILE
+       FD INPUT-FILE
        DATA RECORD IS STUDENT-RECORD.
        01 STUDENT-RECORD.
            05 STUDENT-ID PIC 9(5).
            05 FIRST-NAME PIC A(10).
            05 LAST-NAME PIC A(10).
            05 GRADE PIC 9(3).
-      *>      05 AGE PIC 9(2).
-      *>      05 COURSE PIC A(15).
+           05 AGE PIC 9(2).
+           05 COURSE PIC A(15).
 
        SD WORK-FILE
            DATA RECORD IS WORKING-SORT-RECORD.
@@ -40,13 +44,17 @@
            05 WF-FIRST-NAME PIC A(10).
            05 WF-LAST-NAME PIC A(10).
            05 WF-GRADE PIC 9(3).
+      *>      05 WF-STUDENT-RECORD PIC X(25).
 
-       FD SORTED-FILE
+       FD SORTED-OUTPUT-FILE
            DATA RECORD IS SORTED-STUDENT-RECORD.
        01 SORTED-STUDENT-RECORD.
            05 S-STUDENT-ID PIC 9(5).
+      *>      05 FILLER PIC X(5) VALUE SPACES.
            05 S-STUDENT-FIRST-NAME PIC A(10).
+      *>      05 FILLER PIC X(5) VALUE SPACES.
            05 S-STUDENT-LAST-NAME PIC A(10).
+      *>      05 FILLER PIC X(5) VALUE SPACES.
            05 S-GRADE PIC 9(3).
 
        WORKING-STORAGE SECTION.
@@ -65,29 +73,34 @@
        MAIN-LOGIC.
            SORT WORK-FILE
                ON ASCENDING KEY WF-GRADE
-               USING STUDENTS-FILE
-               GIVING SORTED-FILE
+               USING INPUT-FILE
+               GIVING SORTED-OUTPUT-FILE
       *>          INPUT PROCEDURE IS STUDENT-SORTER-PARA
       *>          OUTPUT PROCEDURE IS SAVE-SORTED-RECORDS.
+      *>             GIVING SORTED-OUTPUT-FILE
        STOP RUN.
 
        STUDENT-SORTER-PARA.
            SET NOT-EOF-INPUT-FILE TO TRUE
 
-           OPEN INPUT STUDENTS-FILE.
-           OPEN OUTPUT SORTED-FILE.
-           READ STUDENTS-FILE
+           OPEN INPUT INPUT-FILE.
+           OPEN OUTPUT SORTED-OUTPUT-FILE.
+           READ INPUT-FILE
                 AT END SET EOF-INPUT-FILE TO TRUE
            END-READ.
 
            DISPLAY "READING UNSORTED RECORDS"
 
-      *>           IF STUDENT-ID = 'S0001' OR STUDENT-ID = 'Student' THEN
-               IF STUDENT-ID = 'S0001' THEN
+
+      *>          IF STUDENT-ID = 'S0001' THEN
                PERFORM UNTIL EOF-OKAY
+
+      *>          IF STUDENT-ID = 'S0001' OR STUDENT-ID = 'Student' THEN
+
                 MOVE STUDENT-RECORD TO WS-SORT-REC
-                DISPLAY "RELEASING TO SORTED-RECORD"
                 RELEASE WORKING-SORT-RECORD
+                DISPLAY "RELEASING TO SORTED-RECORD"
+      *>          END-IF
 
                 IF SORT-RETURN NOT = 0 THEN
                    DISPLAY "SORT ENDED ABNORMALLY. SORT-RETURN = ",
@@ -100,15 +113,15 @@
 
 
        SAVE-SORTED-RECORDS.
-      *>      OPEN OUTPUT SORTED-FILE
+      *>      OPEN OUTPUT SORTED-OUTPUT-FILE
            PERFORM UNTIL EOF-OKAY
                RETURN WORK-FILE
                AT END MOVE 'Y' TO EOF-FLAG
                DISPLAY 'SORT success. SORT-RETURN: ', SORT-RETURN
            END-PERFORM
 
-           CLOSE SORTED-FILE
-           CLOSE STUDENTS-FILE
+           CLOSE SORTED-OUTPUT-FILE
+           CLOSE INPUT-FILE
            .
 
       *>      PERFORM DISPLAY-MENU
